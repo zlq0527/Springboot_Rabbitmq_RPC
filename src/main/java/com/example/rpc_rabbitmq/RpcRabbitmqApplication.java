851608e360b5ce426df197df22256ab3f962fa31
zlq <1142401783@qq.com>
2022-04-14 19:52:12 +0800

    init

---
+++ src/main/java/com/example/rpc_rabbitmq/RpcRabbitmqApplication.java
@@ -0,0 +1,12 @@
+package com.example.rpc_rabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpcRabbitmqApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(RpcRabbitmqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/rpc_rabbitmq/RpcRabbitmqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rpc_rabbitmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpcRabbitmqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
